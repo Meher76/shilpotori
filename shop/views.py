4966d13cb43ed84d4fce0f6148352a23caa2577d
Meher Afroz <22201176@uap-bd.edu>
2025-04-15 00:59:41 +0600

    static

--- shop/views.py
+++ shop/views.py
@@ -7,13 +7,13 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
-    return render(request,template_name='shop\home.html')
+    return render(request,template_name='shop/home.html')
 def products(request):
     posts = Product.objects.all()
     context = {'posts':posts}
-    return render(request,template_name='shop\products.html',context=context)
+    return render(request,template_name='shop/products.html',context=context)
 
 def product_details(request):
-    return render(request,template_name='shop\product_details.html')
+    return render(request,template_name='shop/product_details.html')
 
 
